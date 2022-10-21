7025d5fcb8a3df597d4cf1ecfccee4c7e40ec5fb
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-10-21 17:09:40 +0200

    created map structure, to store info from parser

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 16:14:55 by tmongell          #+#    #+#             */
-/*   Updated: 2022/10/20 19:14:12 by tmongell         ###   ########.fr       */
+/*   Updated: 2022/10/21 17:08:59 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,28 @@
 /*macros----------------------------------------------------------------macros*/
 
 /*structure---------------------------------------------------------structures*/
+
+typedef struct s_map {
+	//display info
+	char	*north_path;
+	char	*south_path;
+	char	*east_path;
+	char	*west_path;
+	void	*north_img;
+	void	*south_img;
+	void	*east_img;
+	void	*west_img;
+	int		floor_color;
+	int		roof_color;
+	//the map itself
+	char	**raw_map;
+	int		**parsed_map;	//put it there, but I'm not sure it's usefull
+	//player info
+	int		start_x;
+	int		start_y;
+	int		start_dir;
+	
+}	t_map;
 
 /*prototypes--------------------------------------------------------prototypes*/
 
