5d801dfa3a4456decc3b269da8732197ddc6c07c
chloe <cmaroude@student.42nice.fr>
2022-12-16 16:27:34 +0100

    ajout sous fonction qui je pense fait le taf que theo a demander

--- srcs/display/ray_utils.c
+++ srcs/display/ray_utils.c
@@ -18,3 +18,16 @@ void	get_ray_angle(t_mlx *mlx, double *angles)
 	while (++i < WIN_W)
 		angles[i] = angles[i -1] + interval;
 }
+
+/* function that get the next x or y on the direction line */
+void    get_next_pos(t_coord *ray, double dir, double slope, double offset)
+{
+	//	y = slope * x + offset;
+	//	from 180 include to 360 non include
+	if (dir > 0 && (dir * (180.0) / M_PI) =< 180.0)
+		ray->x = ray->x - 1;
+	// from 0 include to -180 non include
+	else if (dir =< 0 && (dir * (180.0) / M_PI) > 180.0)
+		ray->x = ray->x + 1;
+	ray->y = (ray->x) - offset / slope;
+}
