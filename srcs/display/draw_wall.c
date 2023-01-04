de1bb96f9ba392b85ed01514e71f88f203fa147d
Theo Mongellaz <theo.mongellaz@gmail.com>
2023-01-04 18:39:54 +0100

    modified draw_wall function so that it does not pose problem with texture integration

--- srcs/display/draw_wall.c
+++ srcs/display/draw_wall.c
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 21:56:48 by tmongell          #+#    #+#             */
-/*   Updated: 2023/01/04 18:09:59 by tmongell         ###   ########.fr       */
+/*   Updated: 2023/01/04 18:38:57 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,20 @@
 
 void	draw_wall(int ray_num, t_wall_data wall, t_img *screen)
 {
-	int	line;
+	int	pos_y;
 	int	pixel_height;
 	int	color_side[4] = {NORTH_C , SOUTH_C , EAST_C , WEST_C};
 	int i;
 
-	line = WIN_H / 2;
+	pos_y = WIN_H / 2;
 	//get number of pixel based on height so that a wall is square when WALL_H=1
 	pixel_height = wall.height * WIN_H; //ce n'est probablement pas cela
-	if (pixel_height >= WIN_H)
-		pixel_height = WIN_H;
-	line -= pixel_height/2;
+	pos_y -= pixel_height/2;
 	i = 0;
 	while (i ++ < pixel_height)
 	{
-		if (line >= 0 && line < WIN_H)
-			img_set_pixel(screen, ray_num, line++, color_side[wall.side - 1]);
+		if (pos_y > 0 && pos_y < WIN_H)
+			img_set_pixel(screen, ray_num, pos_y, color_side[wall.side - 1]);
+		pos_y ++;
 	}
 }
