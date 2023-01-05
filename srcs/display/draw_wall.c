e95dfa665407db4ef4a0b3c9160a1816cb68fa21
Theo Mongellaz <theo.mongellaz@gmail.com>
2023-01-05 19:07:23 +0100

    optimised draw_wall to not draw outside of the window. it help the program draw very tall wall much faster

--- srcs/display/draw_wall.c
+++ srcs/display/draw_wall.c
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 21:56:48 by tmongell          #+#    #+#             */
-/*   Updated: 2023/01/04 18:38:57 by tmongell         ###   ########.fr       */
+/*   Updated: 2023/01/05 19:03:53 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@
 #define SOUTH_C 0xADD8E6 //light blue
 #define EAST_C 0xFFFFE0 //light yellow
 #define WEST_C 0x90EE90 //light green
-*/
+//*/
+///*
 #define NORTH_C 0xFF0000 //red
 #define SOUTH_C 0xFF //blue
 #define EAST_C 0xFFFF00 //yellow
 #define WEST_C 0xFF00 //green
+//*/
 
 void	draw_wall(int ray_num, t_wall_data wall, t_img *screen)
 {
@@ -36,8 +38,9 @@ void	draw_wall(int ray_num, t_wall_data wall, t_img *screen)
 	//get number of pixel based on height so that a wall is square when WALL_H=1
 	pixel_height = wall.height * WIN_H; //ce n'est probablement pas cela
 	pos_y -= pixel_height/2;
-	i = 0;
-	while (i ++ < pixel_height)
+	i = ft_max(0, -pos_y);
+	pos_y = ft_max(0, pos_y);
+	while (i ++ < pixel_height && pos_y < WIN_H)
 	{
 		if (pos_y > 0 && pos_y < WIN_H)
 			img_set_pixel(screen, ray_num, pos_y, color_side[wall.side - 1]);
